a46d93977b86e4c52f001104d5b43aaa4245b9b6
RH <abc@abc.com>
2018-05-25 01:11:07 +0530

    v1

---
+++ Testability/OrderProcessor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Testability
+{
+    class OrderProcessor
+    {
+        private readonly ShippingCalulator _shippingCalulator;
+
+        public OrderProcessor()
+        {
+            _shippingCalulator = new ShippingCalulator();
+        }
+
+        public void Process(Order order)
+        {
+            if (order.IsShipped)
+                throw new InvalidOperationException("This order is already processed.");
+
+            order.Shipment = new Shipment
+            {
+                Cost = _shippingCalulator.CalulateShipping(order),
+                ShippingDate = DateTime.Today.AddDays(1)
+            };
+        }
+    }
+}
